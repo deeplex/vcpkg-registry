729a8473a14ca5a190d83a1e251545035a4ebbe5
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2021-12-14 17:06:48 +0100

    [vefs] Update port @0.4.0-alpha.5

--- ports/vefs/portfile.cmake
+++ ports/vefs/portfile.cmake
@@ -2,7 +2,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL ssh://git@mdk-bs-dev02.test-server.ag:2224/native/vefs.git
-    REF 48c2af3041624a6988e225ccde2a53e9049e9d6b # v0.4.0-alpha.4
+    REF 169f891e525033d40a2ab3230cd178f5855b8014 # v0.4.0-alpha.5
     HEAD_REF develop
 )
 vcpkg_configure_cmake(
