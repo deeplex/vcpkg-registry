46ad87788acbf44d41a8de78fcf45c31be6c49a0
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2022-04-01 22:15:41 +0200

    [status-code] Update to version 1.0.0-20220401
    - Add cxx standard feature levels.

--- ports/status-code/portfile.cmake
+++ ports/status-code/portfile.cmake
@@ -1,25 +1,35 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ned14/status-code
-    REF ab3cd8217b6a89b91708a497f77f119c096fcce3
-    SHA512 266c79875fd6edb4a239768da29401193d5b710fe3d06208df010c5ca56bf80fa4558f572c738ab9290ee378437c882d072e93e8fb10fb1e3787b460f40a7102
+    REF 42d3afaa388ae966938f558d9e00e390a2192fa8
+    SHA512 f7965bfc702473be09a1d29da7e4c6241e481e609d57ff0ad989db7757e2d77b2b6fd1a89882961e8f16c405df97403710f1f3a3d3fd62341703b573d9035726
     HEAD_REF master
     PATCHES
         no-test.patch
+        cxx_feature_level.patch
 )
 
 # Because status-code's deployed files are header-only, the debug build is not necessary
 set(VCPKG_BUILD_TYPE release)
 
-vcpkg_configure_cmake(
-    SOURCE_PATH "${SOURCE_PATH}"
-    PREFER_NINJA
-    OPTIONS
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        cxx14 STATUS_CODE_REQUIRE_CXX14
+        cxx20 STATUS_CODE_REQUIRE_CXX20
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+    MAYBE_UNUSED_VARIABLES
+        STATUS_CODE_REQUIRE_CXX14 # not evaluated in case cxx20 is requested
+)
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/status-code)
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/status-code)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 file(RENAME "${CURRENT_PACKAGES_DIR}/include" "${CURRENT_PACKAGES_DIR}/include2")
