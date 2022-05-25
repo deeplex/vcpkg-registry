105bf73ccae279c35a1ec3dfd54d4c33fdc5fff0
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2022-05-25 09:21:11 +0200

    [outcome] update port @2022-05-25 and support c++20 feature

--- ports/outcome/portfile.cmake
+++ ports/outcome/portfile.cmake
@@ -15,61 +15,54 @@
 # the exact copy of those third party libraries known to
 # have passed Outcome's CI process.
 
-message(WARNING [=[
-Outcome depends on QuickCppLib which uses the vcpkg versions of gsl-lite and byte-lite, rather than the versions tested by QuickCppLib's and Outcome's CI. It is not guaranteed to work with other versions, with failures experienced in the past up-to-and-including runtime crashes. See the warning message from QuickCppLib for how you can pin the versions of those dependencies in your manifest file to those with which QuickCppLib was tested. Do not report issues to upstream without first pinning the versions as QuickCppLib was tested against.
-]=])
+if (NOT "cxx20" IN_LIST FEATURES)
+    message(WARNING [=[
+    Outcome depends on QuickCppLib which uses the vcpkg versions of gsl-lite and byte-lite, rather than the versions tested by QuickCppLib's and Outcome's CI. It is not guaranteed to work with other versions, with failures experienced in the past up-to-and-including runtime crashes. See the warning message from QuickCppLib for how you can pin the versions of those dependencies in your manifest file to those with which QuickCppLib was tested. Do not report issues to upstream without first pinning the versions as QuickCppLib was tested against.
+    ]=])
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ned14/outcome
-    REF a86c6b053b86e4fc5d920907b0afb3234bec9336
-    SHA512 c282afd7b335fef37ec4c2011bf591d3ffd56872515e8fcbd95714020ab99d141a87a064ecda44bf9308cb4bd1f39977b62303f2cc297918be2f257a36ff5832
+    REF 2d2c444e36c2e8dfacfa758d3f3ffb180d9d27b2
+    SHA512 e25d91145cb73aa8f110b2e254c36961ea1e7d36b3456827b217e69ee1c6f485d689287e21f1ef657a0a0abdfbfc52ddb0f607bd38beca0de05a04d693a43dc7
     HEAD_REF develop
     PATCHES
-      outcome-prune-sources.patch
+        fix-find-library.patch
+        fix-status-code-include.patch
 )
 
-# Outcome needs a copy of QuickCppLib with which to bootstrap its cmake
-file(COPY "${CURRENT_INSTALLED_DIR}/include/quickcpplib"
-    DESTINATION "${SOURCE_PATH}/quickcpplib/repo/include/"
-)
-file(COPY "${CURRENT_INSTALLED_DIR}/share/ned14-internal-quickcpplib/"
-    DESTINATION "${SOURCE_PATH}/quickcpplib/repo/"
-)
+# setting CMAKE_CXX_STANDARD here to prevent outcome from messing with compiler flags
+# the cmake package config requires said C++ standard target transitively via quickcpplib
+if ("cxx20" IN_LIST FEATURES)
+    list(APPEND extra_config -DCMAKE_CXX_STANDARD=20)
+elseif("cxx17" IN_LIST FEATURES)
+    list(APPEND extra_config -DCMAKE_CXX_STANDARD=17)
+endif()
 
-# Outcome expects status-code to live inside its include directory
-file(COPY "${CURRENT_INSTALLED_DIR}/include/status-code/"
-    DESTINATION "${SOURCE_PATH}/include/outcome/experimental/status-code/include/"
-)
-file(COPY "${CURRENT_INSTALLED_DIR}/include/status-code/detail/"
-    DESTINATION "${SOURCE_PATH}/include/outcome/experimental/status-code/include/detail/"
-)
-
-# Because outcome's deployed files are header-only, the debug build is not necessary
+# Because status-code's deployed files are header-only, the debug build is not necessary
 set(VCPKG_BUILD_TYPE release)
 
-# Already installed dependencies don't appear on the include path, which Outcome assumes.
-string(APPEND VCPKG_CXX_FLAGS " \"-I${CURRENT_INSTALLED_DIR}/include\"")
-string(APPEND VCPKG_C_FLAGS " \"-I${CURRENT_INSTALLED_DIR}/include\"")
-
 # Use Outcome's own build process, skipping examples and tests.
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    PREFER_NINJA
     OPTIONS
         -DPROJECT_IS_DEPENDENCY=On
-        -Dquickcpplib_FOUND=1
-        -DOUTCOME_ENABLE_DEPENDENCY_SMOKE_TEST=OFF  # Leave this always on to test everything compiles
+        -Dquickcpplib_DIR=${CURRENT_INSTALLED_DIR}/share/quickcpplib
+        -DOUTCOME_BUNDLE_EMBEDDED_STATUS_CODE=OFF
+        -Dstatus-code_DIR=${CURRENT_INSTALLED_DIR}/share/status-code
+        -DOUTCOME_ENABLE_DEPENDENCY_SMOKE_TEST=ON  # Leave this always on to test everything compiles
         -DCMAKE_DISABLE_FIND_PACKAGE_Git=ON
+        ${extra_config}
 )
 
 if("run-tests" IN_LIST FEATURES)
-    vcpkg_build_cmake(TARGET test)
+    vcpkg_cmake_build(TARGET test)
 endif()
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/outcome)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/outcome)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
