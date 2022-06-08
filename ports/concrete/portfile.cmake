67f618c3bc70e1490994004b08d40bb638932d04
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2022-06-08 17:46:58 +0200

    [concrete] Add version 0.0.0-alpha.2

--- ports/concrete/portfile.cmake
+++ ports/concrete/portfile.cmake
@@ -2,9 +2,12 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO deeplex/concrete
-    REF 9ce255f9d1b3b01deb6d82246711aa2e54774e22
-    SHA512 6ed241c52374699f3a00dbc99dacd3834d3a196db8e92c531fa6e7006a53e44c5a6b9f193e3f5968bd2157576bdec0c6472c3b144122c1e4dbd3a83e9e8032cd
+    REF v0.0.0-alpha.2
+    SHA512 6ce6d88eb9d2d878c022f829180ec67b97c4deb218ca3990a59b59c9f29ed583857208b934e233edf5070337038143f46552462d0393ba18885b9cea13142e59
 )
+
+# Because status-code's deployed files are header-only, the debug build is not necessary
+set(VCPKG_BUILD_TYPE release)
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
@@ -14,10 +17,9 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/concrete)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/debug"
     "${CURRENT_PACKAGES_DIR}/lib"
 )
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/concrete RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
