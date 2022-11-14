54769ba6c04ee98c93c03fc50cc1355428c15b4e
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2022-11-14 08:36:56 +0100

    [concrete] Add version 0.0.0-alpha.6

--- ports/concrete/portfile.cmake
+++ ports/concrete/portfile.cmake
@@ -2,15 +2,15 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO deeplex/concrete
-    REF 165617ebfb4f788e22eda6d13b7c8523576bb5bd
-    SHA512 556f4d89e4793341542e9d344541e51d39fbb8687134ef5a92abdf02431ae600a0b49265afe53bd847d55ff176b4a6a6e937af7b471545c2e7432b3d29edcf57
+    REF 941b63214ac0302b10213309f45ecbd59396ea82
+    SHA512 380253b167771dea9d0fafbaa7d817fe422b8191be61475683c483193034bac49fdf785f84cb8d11c93fda8f9377151f1c98ab44d7f06fd66fff5eb70d6bad61
 )
 
 # Because concrete is a header-only library, the debug build is not necessary
 set(VCPKG_BUILD_TYPE release)
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
 )
@@ -22,4 +22,4 @@ file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/lib"
 )
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
