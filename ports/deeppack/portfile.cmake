e0379d3342d009a2ca0b713af9ced5df6e155ada
Henrik S. Gaßmann <henrik@gassmann.onl>
2023-02-02 21:23:08 +0100

    [deeppack] Add version 0.1.0-alpha.2

--- ports/deeppack/portfile.cmake
+++ ports/deeppack/portfile.cmake
@@ -2,22 +2,20 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "deeplex/deeppack"
-    REF v0.0.0-alpha.1
-    SHA512 09348842098e53e91bb90de946d812ca286f2b833ec504d9f890fca5ab497eb3b8d33e9e83e3c83ffc60918e009c92419f4a61af7c7f66c5e18655e31a5ff7cf
-    HEAD_REF master
-    PATCHES
+    REF v0.1.0-alpha.2
+    SHA512 09948d7e9bf0df8ba3df02e591d9ddfe1d2d9ef0dea9578b3763c4c1fbd883ca2123114932a4df8924e115218c6b3bc85d00a10248c9fdcdad8056f4ff812271
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
-     -DBUILD_TESTING=OFF
+        -DBUILD_TESTING=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/deeppack RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
