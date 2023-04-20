f526dd50253c25e39577616d9dd0c1e970bb2ecd
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2023-04-20 12:46:19 +0200

    [boringssl] Backport port from upstream 2023-04-20

---
+++ ports/boringssl/portfile.cmake
@@ -0,0 +1,50 @@
+if(EXISTS "${CURRENT_INSTALLED_DIR}/include/openssl/ssl.h")
+  message(FATAL_ERROR "Can't build BoringSSL if OpenSSL is installed. Please remove OpenSSL, and try to install BoringSSL again if you need it. Build will continue since BoringSSL is a drop-in replacement for OpenSSL")
+endif()
+
+vcpkg_find_acquire_program(PERL)
+get_filename_component(PERL_EXE_PATH ${PERL} DIRECTORY)
+vcpkg_add_to_path(${PERL_EXE_PATH})
+
+vcpkg_find_acquire_program(NASM)
+get_filename_component(NASM_EXE_PATH ${NASM} DIRECTORY)
+vcpkg_add_to_path(${NASM_EXE_PATH})
+
+vcpkg_find_acquire_program(GO)
+get_filename_component(GO_EXE_PATH ${GO} DIRECTORY)
+vcpkg_add_to_path(${GO_EXE_PATH})
+
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO google/boringssl
+  REF bcecc7d834fc44ad257b2f23f88e1cf597ab2736
+  SHA512 29850de6927b76917044fe99922fe0ec6a6288effa574047e391d83cc9d4f8138d0f575f77c9f6c5e0f69a6a9cb8a795a612f9df622acf308ac1b475954b0662
+  HEAD_REF master
+  PATCHES
+    0001-vcpkg.patch
+    0002-remove-WX-Werror.patch
+)
+
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+    ${FEATURE_OPTIONS}
+  OPTIONS_DEBUG
+    # the FindOpenSSL.cmake script differentiates debug and release binaries using this suffix.
+    -DCMAKE_DEBUG_POSTFIX=d
+)
+
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+vcpkg_cmake_config_fixup(PACKAGE_NAME OpenSSL CONFIG_PATH lib/cmake/OpenSSL)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_copy_tools(TOOL_NAMES bssl AUTO_CLEAN)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/install-pc-files.cmake")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
