0c4a1a7836a65b59a59d8921fa442c6da0497104
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2023-07-12 11:28:18 +0200

    [deeplog] Add version 0.0.0-alpha.3

--- ports/deeplog/portfile.cmake
+++ ports/deeplog/portfile.cmake
@@ -3,20 +3,19 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "deeplex/deeplog"
     REF "v${VERSION}"
-    SHA512 67bec78f70ee5979fc468ceff0e29954f32b17e2b73b0a805e22d57a7badc8a840980df3b9315d35f30467051b85a7c43167088a4c7ab4c077c87fcdfa62c104
+    SHA512 df808b319bb693580b72d7a997072ba0c8e5c845d111632b76ed848d394a46ed918e3235dbf4a1d0b1897fdc78b643a6f59221121fe2997790e7cdcde7c7a07e
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
         -DWARNINGS_AS_ERRORS=OFF
+        "-DCMAKE_INSTALL_INCLUDEDIR=${CURRENT_PACKAGES_DIR}/include"
 )
 
 vcpkg_cmake_install()
-
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
