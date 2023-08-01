99240f9b29e0b05cb294407aa5294338550a6f03
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2023-08-01 20:12:36 +0200

    [deeppack] Add version 0.1.0-alpha.8

--- ports/deeppack/portfile.cmake
+++ ports/deeppack/portfile.cmake
@@ -3,20 +3,19 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "deeplex/deeppack"
     REF "v${VERSION}"
-    SHA512 1d9f2ff9b14405a8613bdf0cdcf579d376db540a14efcda894d6f46663294de5ddd9cdb1282ed7516811235c78a1d50d4bddbf18a3890286f245bfc43968e947
+    SHA512 c45a4ed0e5556662074d4cf2395529818ed6df64a4651bceab504b0c817bf7e294a39eaa55bcddbc1e58eb44d89bdf0db4b0aa925bfe97d0ed168ec9622dc2b4
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
         -DWARNINGS_AS_ERRORS=OFF
+        "-DCMAKE_INSTALL_INCLUDEDIR=${CURRENT_PACKAGES_DIR}/include"
 )
 
 vcpkg_cmake_install()
-
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
