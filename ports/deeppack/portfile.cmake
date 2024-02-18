5fcc58303af6541dcc6f4b60159237a6bbb35197
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2024-02-18 14:19:28 +0100

    [deeppack] Update to version 0.1.0-beta.1

--- ports/deeppack/portfile.cmake
+++ ports/deeppack/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "deeplex/deeppack"
     REF "v${VERSION}"
-    SHA512 e8d09d783db698a1ca34a8029dadfa7454a96080537292b7a0302a5a883de5002e7131951a775246d3d38b5ffcdf6ad2fa028fa46cdfaf9f6fe818fa9841a97e
+    SHA512 03b2b5dfe95f16a1cff70b0eb93b98d350ae972f8fbbf1d0a62447c4f7e0c0b76dd626ac0f05e53e2356eae2eb196996c2cfb9d99c0b428b29060fcebae67162
 )
 
 vcpkg_cmake_configure(
